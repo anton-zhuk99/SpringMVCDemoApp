a96f8c58120bb01b906fe82875a893391bba7a34
Anton Zhuk <54962677+anton-zhuk99@users.noreply.github.com>
2020-01-14 18:34:27 +0200

    Update Note class

--- src/main/java/ua/antonzhuk/springmvcdemoapp/model/Note.java
+++ src/main/java/ua/antonzhuk/springmvcdemoapp/model/Note.java
@@ -1,18 +1,28 @@
 package ua.antonzhuk.springmvcdemoapp.model;
 
+import java.sql.Date;
+import java.sql.Time;
+
 public class Note {
 
     private int id;
     private String text;
+    private java.sql.Date date;
+    private Time time;
 
     public Note() {}
 
-    public Note(int id, String text) {
+    public Note(int id, String text, java.sql.Date date, java.sql.Time time) {
         this(text);
         this.id = id;
+        this.date = date;
+        this.time = time;
     }
 
     public Note(String text) {
+        java.util.Date utilDate = new java.util.Date();
+        date = new java.sql.Date(utilDate.getTime());
+        time = new java.sql.Time(utilDate.getTime());
         this.text = text;
     }
 
@@ -30,5 +40,21 @@ public class Note {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
     }
 }
